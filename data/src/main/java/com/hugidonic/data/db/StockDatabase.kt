f7933b1f95ce9220f47f90b99ca14e036024074c
Hugidonic <netvaca@ya.ru>
2023-05-12 10:28:14 +0300

    @Database annotation added

--- data/src/main/java/com/hugidonic/data/db/StockDatabase.kt
+++ data/src/main/java/com/hugidonic/data/db/StockDatabase.kt
@@ -1,7 +1,12 @@
 package com.hugidonic.data.db
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
 
+@Database(
+    entities = [CompanyListingEntity::class],
+    version = 1,
+)
 abstract class StockDatabase: RoomDatabase() {
     abstract val stockDao: StockDao
 }
