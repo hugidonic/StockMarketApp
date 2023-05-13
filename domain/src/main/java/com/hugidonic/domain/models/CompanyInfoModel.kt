7f86a750e0a29c85307ccc80ea568499f3370959
Hugidonic <netvaca@ya.ru>
2023-05-13 19:42:15 +0300

    Company info model & dto

---
+++ domain/src/main/java/com/hugidonic/domain/models/CompanyInfoModel.kt
@@ -0,0 +1,9 @@
+package com.hugidonic.domain.models
+
+data class CompanyInfoModel(
+    val symbol: String,
+    val description: String,
+    val name: String,
+    val country: String,
+    val industry: String,
+)
