7f856153d4191452082d68aa1b5eb8b70ee107b6
Hugidonic <netvaca@ya.ru>
2023-05-13 19:43:08 +0300

    Added Api queries for getIntradayInfo & getCompanyInfo

--- data/src/main/java/com/hugidonic/data/remote/StockApiService.kt
+++ data/src/main/java/com/hugidonic/data/remote/StockApiService.kt
@@ -1,5 +1,6 @@
 package com.hugidonic.data.remote
 
+import com.hugidonic.data.remote.dto.CompanyInfoDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,4 +11,16 @@ interface StockApiService {
     suspend fun getListings(
         @Query("apikey") apiKey: String = ApiFactory.API_KEY
     ): ResponseBody
+
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntradayInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = ApiFactory.API_KEY,
+    ): ResponseBody
+
+    @GET("query?function=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = ApiFactory.API_KEY,
+    ): CompanyInfoDto
 }
