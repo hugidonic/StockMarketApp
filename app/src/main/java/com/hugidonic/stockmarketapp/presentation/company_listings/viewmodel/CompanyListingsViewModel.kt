5b19281f7c71f1ac9393aa92fad07ba8eaf18a1c
Hugidonic <netvaca@ya.ru>
2023-05-15 22:13:47 +0300

    CompanyListingsViewModel on init gets company listings

--- app/src/main/java/com/hugidonic/stockmarketapp/presentation/company_listings/viewmodel/CompanyListingsViewModel.kt
+++ app/src/main/java/com/hugidonic/stockmarketapp/presentation/company_listings/viewmodel/CompanyListingsViewModel.kt
@@ -24,6 +24,10 @@ class CompanyListingsViewModel @Inject constructor(
 
     private var searchJob: Job? = null
 
+    init {
+        getCompanyListings()
+    }
+
     fun onEvent(event: CompanyListingsEvent) {
         when(event) {
             is CompanyListingsEvent.Refresh -> {
